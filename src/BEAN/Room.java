ad3594d25a71f8ce99f97584bdbe270d9aa0a140
guotao <525549063@qq.com>
2019-07-03 15:06:51 +0800

    添加了room 和js

---
+++ src/BEAN/Room.java
@@ -0,0 +1,58 @@
+package BEAN;
+
+public class Room {
+    private String room_id;
+    private String room_type;
+    private String room_state;
+    private String room_price;
+    private String room_location;
+    private String room_no;
+
+    public String getRoom_id() {
+        return room_id;
+    }
+
+    public void setRoom_id(String room_id) {
+        this.room_id = room_id;
+    }
+
+    public String getRoom_type() {
+        return room_type;
+    }
+
+    public void setRoom_type(String room_type) {
+        this.room_type = room_type;
+    }
+
+    public String getRoom_state() {
+        return room_state;
+    }
+
+    public void setRoom_state(String room_state) {
+        this.room_state = room_state;
+    }
+
+    public String getRoom_price() {
+        return room_price;
+    }
+
+    public void setRoom_price(String room_price) {
+        this.room_price = room_price;
+    }
+
+    public String getRoom_location() {
+        return room_location;
+    }
+
+    public void setRoom_location(String room_location) {
+        this.room_location = room_location;
+    }
+
+    public String getRoom_no() {
+        return room_no;
+    }
+
+    public void setRoom_no(String room_no) {
+        this.room_no = room_no;
+    }
+}
