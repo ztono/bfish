85a54a2b26bc7e6f9ffc807f39ce4e77a7d479a0
4ilde <wilde08@163.com>
2019-07-05 17:00:59 +0800

    换房

--- src/Servlet/changeRoomServlet.java
+++ src/Servlet/changeRoomServlet.java
@@ -9,5 +9,12 @@ public class changeRoomServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         doPost(request,response);
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        String client_no = request.getParameter("client_no");
+        String or_room_id = request.getParameter("room_id1");
+        String to_room_id = request.getParameter("room_id2");
+
     }
 }
