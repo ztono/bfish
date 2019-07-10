024b7fc078625dffc7323b6e9eb6761793ecd8d9
ztono <947142043@qq.com>
2019-07-10 14:49:45 +0800

    预定DAO

--- src/DAO/QueryDAO.java
+++ src/DAO/QueryDAO.java
@@ -1,19 +1,111 @@
 package DAO;
 
 import java.sql.ResultSet;
-import java.util.Date;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author JunzhengChen
  * Create Time 2019/7/10 0:57
  */
-public class QueryDAO {
+public class QueryDAO extends DAO.BaseDao {
 
-    public static int getRoomRemain(Date queryDate){
+    public QueryDAO() {
 
-        String[] roomType = {"singleRoom","doubleRoom","bigbedroom"};
+    }
 
-        return 1;
+    public ArrayList<Integer> getRoomRemain(String queryDate) {
+        int singleRoomNumber = 0;
+        int doubleRoomNumber = 0;
+        int bigbedroomNumber = 0;
+        ArrayList<Integer> result=new ArrayList<Integer>();
+        String[] roomType = {"singleRoom", "doubleRoom", "bigbedroom"};
+        //Get all room
+        ResultSet roomResult;
+        String roomSql = "select room_type,room_no from room";
+        try {
+            List<Object> params = new ArrayList<Object>();
+            roomResult = executeQuery(roomSql, params);
+            while (roomResult.next()) {
+                String roomNo = roomResult.getString("room_no");
+                if(checkCheckin(queryDate,roomNo)&&checkReserve(queryDate,roomNo)){
+                    if("singleroom".equals(roomResult.getString("room_type"))){
+                        singleRoomNumber+=1;
+                    }
+                    if("doubleroom".equals(roomResult.getString("room_type"))){
+                        doubleRoomNumber+=1;
+                    }
+                    if("bigbedroom".equals(roomResult.getString("room_type"))){
+                        bigbedroomNumber+=1;
+                    }
+                }
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        result.add(singleRoomNumber);
+        result.add(doubleRoomNumber);
+        result.add(bigbedroomNumber);
+        return result;
+    }
+
+    /**
+     * 根据入住判断该房是否可用
+     *
+     * @param queryDate 查询的日期
+     * @param roomNo    查询的房间
+     * @return
+     */
+    public boolean checkCheckin(String queryDate, String roomNo) {
+        ResultSet resultSet;
+        String sql = "Select * from checkin where room_no=? and arrivedate<=? and leavedate>?";
+        try {
+            List<Object> params = new ArrayList<Object>();
+            params.add(roomNo);
+            params.add(queryDate);
+            params.add(queryDate);
+            resultSet = executeQuery(sql, params);
+            if (resultSet.next()) {
+                return false;
+            } else {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 根据预约判断该房是否可用
+     *
+     * @param queryDate
+     * @param roomNo
+     * @return
+     */
+    public boolean checkReserve(String queryDate, String roomNo) {
+        ResultSet resultSet;
+        String sql = "Select * from reserve where room_no=? and orderarrivedate<=? and orderleavedate>?";
+        try {
+            List<Object> params = new ArrayList<Object>();
+            params.add(roomNo);
+            params.add(queryDate);
+            params.add(queryDate);
+            resultSet = executeQuery(sql, params);
+            if (resultSet.next()) {
+                return false;
+            } else {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
 
